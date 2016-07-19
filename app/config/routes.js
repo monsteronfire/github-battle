335217a48a2fb01f9db79dcfade74ad8b82090b8
monsteronfire <monsteronfire@gmail.com>
2016-07-19 09:31:37 +0800

    Remove Avatar component code * Also add routes to router.js

---
+++ app/config/routes.js
@@ -0,0 +1,22 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+
+var routes = (
+  <Router>
+    <Route path='/' component={Main}>
+      <Route path='/home' component={Home} />
+      <Route path='playerOne' header='Player One' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
+      <Route path='battle' component={ConfirmBattleContainer} />
+      <Route path='results' component={ResultsContainer} />
+    </Route>
+  </Router>
+);
+
+module.exports = routes;
