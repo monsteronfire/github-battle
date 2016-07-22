09f4c029a706e7757f5bd9cfcf7e29f9ee8ae4ff
monsteronfire <monsteronfire@gmail.com>
2016-07-23 08:38:05 +1000

    Refacter code to make Prompt a stateless functional component * Also move UI rendering from PromptContainer to Prompt

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,7 +9,7 @@ var Home = React.createClass({
     return (
         <div className='jumbotron col-sm-12 text-center' style={transparentBg}>
           <h1>Github Battle</h1>
-          <p class="lead">Some fancy motto</p>
+          <p className="lead">Some fancy motto</p>
           <Link to='/playerOne'>
             <button type='button' className='btn btn-lg btn-success'>Get Started</button>
           </Link>
--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,51 +1,55 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt');
+
+//PromptContainer is a container component
+//A container component holds the business logic i.e. routes and such
+//app/component/Prompt.js is a presentational component
+//A presentational component will render UI
 
 var PromptContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
   getInitialState: function() {
     return {
       username: ''
     }
   },
 
-  onUpdateUser: function(e) {
+  handleUpdateUser: function(e) {
     this.setState({
-      username: 'e.target.value'
+      username: e.target.value
     })
   },
 
-  onSubmitUser: function(a) {
+  handleSubmitUser: function(e) {
     e.preventDefault();
     var username = this.state.username;
     this.setState({
       username: ''
-    })
+    });
+
+    if(this.props.routeParams.playerOne){
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+    } else {
+      this.context.router.push('/playerTwo/' + this.state.username)
+    }
   },
 
   render: function() {
     return (
-      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-        <h1>{this.props.route.header}</h1>
-        <div className="col-sm-12">
-          <form onSubmit={this.onSubmitUser}>
-            <div className="form-group">
-              <input
-                className="form-control"
-                placeholder="Github Username"
-                onChange={this.onUpdateUser}
-                value={this.state.username}
-                type="text" />
-            </div>
-            <div className="form-group col-sm-4 col-sm-offset-4">
-              <button
-                className="btn btn-block btn-success"
-                type="submit">
-                  Continue
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <Prompt
+         onSubmitUser = {this.handleSubmitUser}
+         onUpdateUser = {this.handleUpdateUser}
+         header = {this.props.route.header}
+         username = {this.state.username} />
     )
   }
 });
