36b297fde4348db3d35f5cc7618ce87fc9387ab5
monsteronfire <monsteronfire@gmail.com>
2016-07-28 07:58:21 +0800

    Add initial UI for results page

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
 
 function puke(object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -7,7 +8,25 @@ function puke(object) {
 function ConfirmBattle(props) {
   return props.isLoading === true
     ? <p>LOADING</p>
-    : <div>CONFIRM BATTLE: {puke(props)} </div>
+    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className="col-sm-8 col-sm-offset-2">
+          <div className="col-sm-6">
+            <p className="lead">Player 1</p>
+            PLAYER ONE INFO
+          </div>
+          <div className="col-sm-6">
+            <p className="lead">Player 2</p>
+            PLAYER TWO INFO
+          </div>
+        </div>
+      </div>
+}
+
+ConfirmBattle.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired,
+  playersInfo: PropTypes.array.isReuired
 }
 
 module.exports = ConfirmBattle;
