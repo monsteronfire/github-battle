e77fb34a0ce43b14efd388491b8842f87fbb219f
monsteronfire <monsteronfire@gmail.com>
2016-08-01 09:23:57 +0800

    Add UserDetailsWrapper

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -15,14 +15,12 @@ function ConfirmBattle(props) {
     : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
         <h1>Confirm Players</h1>
         <div className="col-sm-8 col-sm-offset-2">
-          <div className="col-sm-6">
-            <p className="lead">Player 1</p>
+          <UserDetailsWrapper>
             <UserDetails info={props.playersInfo[0]} />
-          </div>
-          <div className="col-sm-6">
-            <p className="lead">Player 2</p>
+          </UserDetailsWrapper>
+          <UserDetailsWrapper>
             <UserDetails info={props.playersInfo[1]} />
-          </div>
+          </UserDetailsWrapper>
         </div>
         <div className="col-sm-8 col-sm-offset-2">
           <div className="col-sm-12" style={styles.space}>
