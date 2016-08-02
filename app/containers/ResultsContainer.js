f54463f4122883c30b46307886c1ceeaf440a5a8
monsteronfire <monsteronfire@gmail.com>
2016-08-02 23:13:54 +0800

    Add ResultsContainer to router

--- app/containers/ResultsContainer.js
+++ app/containers/ResultsContainer.js
@@ -1,9 +1,10 @@
 var React = require('react');
+var Results = require('../components/Results');
 
 var ResultsContainer = React.createClass({
   render: function () {
     return (
-      <div />
+      <Results />
     );
   }
 });
