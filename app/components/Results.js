0542032670181287efc797f6f1e4c9e99b03557c
monsteronfire <monsteronfire@gmail.com>
2016-08-06 11:29:44 +0800

    Add MainContainer

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,30 +4,39 @@ var styles = require('../styles');
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var Link = require('react-router').Link;
+var MainContainer = require('./MainContainer');
 
-function dump(object) {
-  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+function StartOver() {
+  return (
+    <div className="col-sm-12" style={styles.space}>
+      <Link to="/playerOne">
+        <button type="button" class="btn btn-lg btn-danger">Start Over</button>
+      </Link>
+    </div>
+  )
 }
 
 function Results(props) {
+  if (props.loading === true) {
+    return (
+      <p>Loading</p>
+    )
+  }
+
   var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
   var losingIndex = winningIndex === 0 ? 1 : 0;
 
   if (props.scores[0] === props.scores[1]) {
     return (
-      <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      <MainContainer>
         <h1>It's A Tie!</h1>
-        <div className="col-sm-12" style={styles.space}>
-          <Link to="/playerOne">
-            <button type="button" class="btn btn-lg btn-danger">Start Over</button>
-          </Link>
-        </div>
-      </div>
+        <StartOver />
+      </MainContainer>
     )
   }
 
   return (
-    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+    <MainContainer>
       <h1>Results</h1>
       <div className="col-sm-8 col-sm-offset-2">
         <UserDetailsWrapper header="Winner">
@@ -41,13 +50,8 @@ function Results(props) {
           </UserDetails>
         </UserDetailsWrapper>
       </div>
-
-      <div className="col-sm-12" style={styles.space}>
-        <Link to='/playerOne'>
-          <button type="button" className="btn btn-lg btn-danger">Start Over</button>
-        </Link>
-      </div>
-    </div>
+      <StartOver />
+    </MainContainer>
   )
 }
 
