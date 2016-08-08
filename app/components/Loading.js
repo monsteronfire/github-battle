c69cd5f0ad161ad315953c5d024de0b5dcf9ef15
monsteronfire <monsteronfire@gmail.com>
2016-08-08 09:08:13 +0800

    Finish reusable loading component

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,23 +1,61 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+
+var styles = {
+  container: {
+    position: 'fixed',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    fontSize: '55px'
+  },
+  content: {
+    textAlign: 'center',
+    position: 'absolute',
+    width: '100%',
+    marginTop: '30px',
+  }
+}
 
 var Loading = React.createClass({
+  propTypes: {
+    text: PropTypes.string,
+    speed: PropTypes.number
+  },
+
+  getDefaultProps: function() {
+    return {
+      text: 'Loading',
+      speed: 300
+    }
+  },
+
   getInitialState: function(){
-    this.originalText = 'Loading';
+    this.originalText = this.props.text;
 
     return {
-      text: 'Loading'
+      text: this.originalText
     }
   },
 
   componentDidMount: function() {
-    var stopper = 'Loading...';
+    var stopper = this.originalText + '...';
     this.interval = setInterval(function() {
       if(this.state.text === stopper) {
         this.setState({
-          text: 'Loading'
+          text: this.originalText
+        })
+      } else {
+        this.setState({
+          text: this.state.text + '.'
         })
       }
-    })
+    }.bind(this), this.props.speed)
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
   },
 
   render: function() {
